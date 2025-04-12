0a50250b6aa7c1a11e02b22565352b2203042dc1
happyhzr <insisthzr@163.com>
2025-04-12 16:09:46 +0800

    create products

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import productRoutes from './routes/productRoutes.js';
 import { sql } from './config/db.js';
@@ -12,10 +13,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
 app.use(morgan('dev'));
 app.use(async (req, res, next) => {
     try {
@@ -42,6 +46,13 @@ app.use(async (req, res, next) => {
 });
 
 app.use('/api/v1/products', productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    // app.use(express.static(path.join(__dirname, '../frontend/dist')))
+    // app.get('*', (req, res) => {
+    //    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    // })
+}
 
 const initDB = async function () {
     try {
